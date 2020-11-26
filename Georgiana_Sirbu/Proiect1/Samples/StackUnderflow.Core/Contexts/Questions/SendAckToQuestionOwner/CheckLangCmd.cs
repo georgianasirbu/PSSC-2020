40fb5737a4e9533a3d2f0a71c183afb0e299d1f6
georgianasirbu <georgianasirbu19@gmail.com>
2020-11-26 15:11:51 +0200

    Partea1

---
+++ Georgiana_Sirbu/Proiect1/Samples/StackUnderflow.Core/Contexts/Questions/SendAckToQuestionOwner/CheckLangCmd.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StackUnderflow.Domain.Core.Contexts.Question.SendAckToQuestionOwner
+{
+    public class CheckLangCmd
+    {
+	[Required]
+        public string Text { get;private set; }
+        
+        public CheckLangCmd(string tet)
+        {
+            Text=text;
+	}
+    }
+}
---
+++ Georgiana_Sirbu/Proiect1/Samples/StackUnderflow.Schema/Models/QuestionBody.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StackUnderflow.DatabaseModel.Models
+{
+    public class QuestionBody
+    {
+        public int QuestionId { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public string Tags { get; set; }
+    }
+}
