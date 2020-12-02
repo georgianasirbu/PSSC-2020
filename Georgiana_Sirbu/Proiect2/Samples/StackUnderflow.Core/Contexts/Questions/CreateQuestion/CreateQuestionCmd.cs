01602d4c064652c9ea5fa3e3e42aa9214f6d1d52
georgianasirbu <georgianasirbu19@gmail.com>
2020-12-03 00:47:53 +0200

    Partea2

---
+++ Georgiana_Sirbu/Proiect2/Samples/StackUnderflow.Core/Contexts/Questions/CreateQuestion/CreateQuestionCmd.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace StackUnderflow.Domain.Core.Contexts.Question.CreateQuestion
+{
+    public struct CreateQuestionCmd
+    {
+
+        [Required]
+        public string Title { get; private set; }
+
+            
+        [Required]
+        public string Description { get; private set; }
+
+            
+        [Required]            
+        public string Tag { get; private set; }
+   
+        public CreateQuestionCmd(string title, strind description, string tag)           
+        {
+            Title = title;              
+            Description = description;              
+            Tag = tag;            
+        }
+
+       
+    }
+    
+}
---
+++ Georgiana_Sirbu/Proiect2/Samples/StackUnderflow.Core/Contexts/Questions/SendAckToQuestionOwner/SendAckToQuestionOwnerResult.cs
@@ -0,0 +1,40 @@
+﻿using CSharp.Choices;
+using StackUnderflow.EF.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StackUnderflow.Domain.Core.Contexts.Question.SendAckToQuestionOwner
+{
+    [AsChoice]
+    public static partial class SendAckToQuestionOwnerResult
+    {
+        public interface ISendAckToQuestionOwnerResult { }
+
+        public class SendAck : ISendAckToQuestionOwnerResult
+        {
+            public int QuestionID { get; }
+	    public string Title {get;}
+	    public string Description {get;}
+	    public string Tag{get;}
+	public SendAck(int questionID, string title, string description, string tag) 
+	{
+	QuestionID=questionID;
+	Title=title;
+	Description=description;
+	Tag=tag;
+    }
+}
+
+        public class NotSentAck : ISendAckToQuestionOwnerResult
+        {
+            public string Msg { get; }
+
+            public NotSentAck (string msg)
+            {
+                Msg = msg;
+            }
+
+        }
+    }
+}
